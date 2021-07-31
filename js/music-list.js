b7c676f002f68425991c1d7efa3ced931c6283e0
kea20055 <81277449+kea20055@users.noreply.github.com>
2021-07-30 22:35:23 -0400

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,129 @@
+let allMusic = [
+    {
+      name: "Yonaguni",
+      artist: "Bad Bunny",
+      img: "yonaguni",
+      src: "yonaguni"
+    },
+    {
+      name: "Si estuviesemos juntos",
+      artist: "Bad Bunny",
+      img: "juntos",
+      src: "si estuviesemos juntos"
+    },
+    {
+      name: "La canción",
+      artist: "Bad Bunny",
+      img: "la cancion",
+      src: "la cancion"
+    },
+    {
+      name: "Callaita",
+      artist: "Bad Bunny",
+      img: "callaita",
+      src: "callaita"
+    },
+    {
+      name: "Dakiti",
+      artist: "Bad Bunny",
+      img: "dakiti",
+      src: "dakiti"
+    },
+    {
+      name: "La noche de anoche",
+      artist: "Bad Bunny",
+      img: "la noche de anoche",
+      src: "la noche de anoche"
+    },
+    {
+      name: "Vete",
+      artist: "Bad Bunny",
+      img: "vete",
+      src: "vete"
+    },
+    {
+    name: "Amorfoda",
+    artist: "Bad Bunny",
+    img: "amorfoda",
+    src: "amorfoda"
+    },
+    {
+    name: "Mia",
+    artist: "Bad Bunny",
+    img: "Mia",
+    src: "Mia"
+    },
+    {
+    name: "Si veo a tu mama",
+    artist: "Bad Bunny",
+    img: "si veo a tu mama",
+    src: "si veo a tu mama"
+    },
+    {
+    name: "Otra noche en miami",
+    artist: "Bad Bunny",
+    img: "otra noche en miami",
+    src: "otra noche en miami"
+    },
+    {
+    name: "Haciendo que me amas",
+    artist: "Bad Bunny",
+    img: "haciendo que me amas",
+    src: "haciendo que me amas"
+    },
+    {
+    name: "Ignorantes",
+    artist: "Bad Bunny",
+    img: "ignorantes",
+    src: "ignorantes"
+    },
+    {
+    name: "Un dia",
+    artist: "Bad Bunny",
+    img: "un dia",
+    src: "un dia"
+    },
+    {
+    name: "La santa",
+    artist: "Bad Bunny",
+    img: "la santa",
+    src: "la santa"
+},
+{
+    name: "Antes de que se acabe",
+    artist: "Bad Bunny",
+    img: "antes de que se acabe",
+    src: "antes de que se acabe"
+},
+{
+    name: "Solo de mi",
+    artist: "Bad Bunny",
+    img: "solo de mi",
+    src: "solo de mi"
+},
+{
+    name: "La dificil",
+    artist: "Bad Bunny",
+    img: "la dificil",
+    src: "la dificil"
+},
+{
+    name: "A tu merced",
+    artist: "Bad Bunny",
+    img: "a tu merced",
+    src: "a tu merced"
+},
+{
+    name: "Te mudaste",
+    artist: "Bad Bunny",
+    img: "te mudaste",
+    src: "te mudaste"
+},
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
